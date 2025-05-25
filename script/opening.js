66f6963ff0d59cd4ad17f42badd746468b134e85
Ramon Batista Varjão <emailderamonbv@gmail.com>
2025-05-25 20:27:31 -0300

    Section: Contact/responsiveness

--- script/opening.js
+++ script/opening.js
@@ -17,3 +17,22 @@ window.onclick = function(event) {
         modal.classList.remove("active");
     }
 };
+
+function copy(text) {
+    navigator.clipboard.writeText(text);
+
+    const message = document.getElementById('copied');
+    message.classList.add('show')
+
+    setTimeout(() => {
+        message.classList.remove('show');
+    }, 1500);
+}
+
+document.getElementById('copy-email').addEventListener('click', () => {
+    copy('emailderamonbv@gmail.com')
+});
+
+document.getElementById('copy-phone').addEventListener('click', () => {
+    copy('+55 12 99224-5370')
+});
